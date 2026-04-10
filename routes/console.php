ed3fdd125e573f1bb49fa98a18e82c402d33e610
Rafa Escobar <rafael.eg.code@gmail.com>
2026-04-10 15:58:37 -0600

    Agrega schedule

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Jobs\SyncRssFeed;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new SyncRssFeed)
+    ->dailyAt('01:00')
+    ->withoutOverlapping();
